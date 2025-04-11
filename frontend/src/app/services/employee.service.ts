3338258a8bd8acf8b59f18b4d170c3a95a60d055
LevanBedinashvili <levanibedinashvili03@gmail.com>
2025-04-11 15:01:30 +0400

    fix error

--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Employee, ApiResponse } from '../models/employee.model';
+import { Employee ,ApiResponse } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +11,8 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) {}
 
-  getAllEmployees(): Observable<ApiResponse<Employee[]>> {
-    return this.http.get<ApiResponse<Employee[]>>(this.apiUrl);
+  getEmployees(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
   }
 
   addEmployee(employee: Employee): Observable<ApiResponse<Employee>> {
